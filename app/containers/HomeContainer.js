3e4691a6b28372cc5e47eebe7f0ed121f0aa9185
Jon Small <jonssmall@gmail.com>
2016-10-15 21:11:32 -0400

    basic route

---
+++ app/containers/HomeContainer.js
@@ -0,0 +1,10 @@
+var React = require('react');
+// var Home = require('../components/Home');
+
+var HomeContainer = React.createClass({
+  render: function () {
+    return <p>Hello from HomeContainer</p>
+  }
+});
+
+module.exports = HomeContainer;
