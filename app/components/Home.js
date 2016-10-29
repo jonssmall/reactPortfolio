6b361c807937254a9f546271f3841fdcd7d661d3
Jon Small <jonssmall@gmail.com>
2016-10-29 00:54:14 -0400

    experimental tab UI. won't scale to add projects. needs a responsive hamburger nav. looks bad on phone

--- app/components/Home.js
+++ app/components/Home.js
@@ -5,24 +5,62 @@ var ShortenerContainer = require('../containers/ShortenerContainer');
 var CalculatorContainer = require('../containers/CalculatorContainer');
 var TicTacToeContainer = require('../containers/TicTacToeContainer');
 
+// function Home(props) {
+//     return (
+//         <div className="mdl-grid">
+//             <div className="mdl-cell mdl-cell--4-col">
+//                 <div className="mdl-cell">
+//                     <About />
+//                 </div>                
+//                 <div className="mdl-cell">
+//                     <ShortenerContainer />
+//                 </div>                                                                                       
+//             </div>
+//             <div className="mdl-cell mdl-cell--8-col">
+//                 <div className="mdl-cell">
+//                     <CalculatorContainer />
+//                 </div>
+//                 <div className="mdl-cell">
+//                     <TicTacToeContainer />
+//                 </div>      
+//             </div>
+//         </div>
+//     )
+// };
+
 function Home(props) {
     return (
         <div className="mdl-grid">
-            <div className="mdl-cell mdl-cell--4-col">
-                <div className="mdl-cell">
-                    <About />
-                </div>                
-                <div className="mdl-cell">
-                    <ShortenerContainer />
-                </div>                                                                                       
+            <div className="mdl-cell mdl-cell--4-col">                                                                                   
             </div>
-            <div className="mdl-cell mdl-cell--8-col">
-                <div className="mdl-cell">
-                    <CalculatorContainer />
-                </div>
-                <div className="mdl-cell">
-                    <TicTacToeContainer />
-                </div>      
+            <div className="mdl-cell mdl-cell--4-col">
+                <div className="mdl-tabs mdl-js-tabs mdl-js-ripple-effect">
+                    <div className="mdl-tabs__tab-bar">
+                        <a href="#about" className="mdl-tabs__tab is-active">About</a>
+                        <a href="#calculator" className="mdl-tabs__tab">Calculator</a>
+                        <a href="#tic-tac-toe" className="mdl-tabs__tab">TicTacToe</a>                        
+                    </div>
+
+                    <div className="mdl-tabs__panel is-active" id="about">
+                        <div className="mdl-grid">
+                            <div className="mdl-cell mdl-cell--12-col">
+                                <About />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="mdl-tabs__panel" id="calculator">
+                        <div className="mdl-cell">
+                            <CalculatorContainer />
+                        </div>
+                    </div>
+                    <div className="mdl-tabs__panel" id="tic-tac-toe">
+                        <div className="mdl-cell">
+                            <TicTacToeContainer />
+                        </div>
+                    </div>                    
+                </div>                
+            </div>
+            <div className="mdl-cell mdl-cell--4-col">                
             </div>
         </div>
     )
