a27f461f31f492ace55ac2cf5357e2c3eee7e446
Jon Small <jonssmall@gmail.com>
2016-10-29 15:12:04 -0400

    comment cleanup w/ placeholder

--- app/components/Home.js
+++ app/components/Home.js
@@ -5,29 +5,6 @@ var ShortenerContainer = require('../containers/ShortenerContainer');
 var CalculatorContainer = require('../containers/CalculatorContainer');
 var TicTacToeContainer = require('../containers/TicTacToeContainer');
 
-// function Home(props) {
-//     return (
-//         <div className="mdl-grid">
-//             <div className="mdl-cell mdl-cell--4-col">
-//                 <div className="mdl-cell">
-//                     <About />
-//                 </div>                
-//                 <div className="mdl-cell">
-//                     <ShortenerContainer />
-//                 </div>                                                                                       
-//             </div>
-//             <div className="mdl-cell mdl-cell--8-col">
-//                 <div className="mdl-cell">
-//                     <CalculatorContainer />
-//                 </div>
-//                 <div className="mdl-cell">
-//                     <TicTacToeContainer />
-//                 </div>      
-//             </div>
-//         </div>
-//     )
-// };
-
 function Home(props) {
     return (
         <div className="mdl-grid">
@@ -45,6 +22,7 @@ function Home(props) {
                         <div className="mdl-grid">
                             <div className="mdl-cell mdl-cell--12-col">
                                 <About />
+                                 { /* <ShortenerContainer /> */ }
                             </div>
                         </div>
                     </div>
